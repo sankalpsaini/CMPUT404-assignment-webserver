3572863a9ed6ea7c547a8c964f013eea81fcf185
Sankalp Saini <61366403+sankalpsaini@users.noreply.github.com>
2023-01-26 21:40:03 -0700

    finished for real

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 #  coding: utf-8 
 import socketserver, os
 
+# License 2023 Sankalp Saini, ssaini
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
